1ee7300eb9910a59c717bc677976cef587093f8c
alien-agent <the_denis@icloud.com>
2021-10-27 18:18:09 +0300

    Fix code formatting in entity/transaction.go

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -8,8 +8,8 @@ import (
 
 type Transaction struct {
 	Id              int64     `json:"id,omitempty" db:"pk"`
-	SenderId        uuid.UUID    `json:"sender_id,omitempty"`
-	RecipientId     uuid.UUID    `json:"recipient_id,omitempty"`
+	SenderId        uuid.UUID `json:"sender_id,omitempty"`
+	RecipientId     uuid.UUID `json:"recipient_id,omitempty"`
 	Amount          int64     `json:"amount"`
 	Description     string    `json:"description"`
 	TransactionDate time.Time `json:"transaction_date,omitempty"`
