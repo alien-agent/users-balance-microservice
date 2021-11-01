66bd5b33f9eca1efdbce1872db79c912da462e3d
alien-agent <the_denis@icloud.com>
2021-11-01 11:51:18 +0300

    Add descriptions for Deposit and Transaction.

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -6,11 +6,21 @@ import (
 	"github.com/google/uuid"
 )
 
+// Transaction represents a money transfer in our bank system.
+// If a Transaction is missing a RecipientId, it is considered a withdrawal transaction (payment for internal or external service).
+// If a Transaction is missing a SenderId, it is considered a top-up transaction.
+// Otherwise, a Transaction is considered a money transfer between two users within the system.
 type Transaction struct {
-	Id              int64     `json:"id,omitempty" db:"pk"`
-	SenderId        uuid.UUID `json:"sender_id,omitempty"`
-	RecipientId     uuid.UUID `json:"recipient_id,omitempty"`
-	Amount          int64     `json:"amount"`
-	Description     string    `json:"description"`
+	// Database id of this Transaction.
+	Id int64 `json:"id,omitempty" db:"pk"`
+	// UUID of sender's Deposit or nil.
+	SenderId uuid.UUID `json:"sender_id,omitempty"`
+	// UUID of recipient's Deposit or nil.
+	RecipientId uuid.UUID `json:"recipient_id,omitempty"`
+	// An amount of rubles subtracted from sender's deposit and added to recipient's deposit. Positive.
+	Amount int64 `json:"amount"`
+	// The description of this Transaction. Optional.
+	Description string `json:"description"`
+	// The date and time when this Transaction was made.
 	TransactionDate time.Time `json:"transaction_date,omitempty"`
 }
