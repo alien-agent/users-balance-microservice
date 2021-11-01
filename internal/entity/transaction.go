d3ea0110201ef7dd3658f410a00c687507bece02
alien-agent <the_denis@icloud.com>
2021-11-01 12:00:05 +0300

    Updated Transaction description.

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -6,16 +6,20 @@ import (
 	"github.com/google/uuid"
 )
 
-// Transaction represents a money transfer in our bank system.
-// If a Transaction is missing a RecipientId, it is considered a withdrawal transaction (payment for internal or external service).
-// If a Transaction is missing a SenderId, it is considered a top-up transaction.
-// Otherwise, a Transaction is considered a money transfer between two users within the system.
+// Transaction represents a single change in user's Deposit.
+//
+// SenderId and RecipientId are "positional". The transaction Amount (which is positive) is always subtracted from
+// sender's deposit and added to recipient's deposit.
+//
+// If a transaction is missing a RecipientId, it is considered a deposit withdrawal.
+// If a transaction is missing a SenderId, it is considered a deposit top-up.
+// Otherwise, a transaction is considered a money transfer between two users within the system.
 type Transaction struct {
 	// Database id of this Transaction.
 	Id int64 `json:"id,omitempty" db:"pk"`
-	// UUID of sender's Deposit or nil.
+	// UUID of sender's Deposit. Optional.
 	SenderId uuid.UUID `json:"sender_id,omitempty"`
-	// UUID of recipient's Deposit or nil.
+	// UUID of recipient's Deposit. Optional.
 	RecipientId uuid.UUID `json:"recipient_id,omitempty"`
 	// An amount of rubles subtracted from sender's deposit and added to recipient's deposit. Positive.
 	Amount int64 `json:"amount"`
