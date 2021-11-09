42dcdf0b3cd440c314ad89ae6e075b252f5b1c65
alien-agent <the_denis@icloud.com>
2021-11-09 13:54:20 +0300

    Replace INT with BIGINT

--- postgres-init.sql
+++ postgres-init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Deposit(
     owner_id UUID PRIMARY KEY,
-    balance INT NOT NULL,
+    balance BIGINT,
 
     CONSTRAINT chk_balance_not_negative
     CHECK(balance >= 0) /* super-safe :) */
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS Transaction(
     id serial PRIMARY KEY,
     sender_id UUID NULL,
     recipient_id UUID NULL,
-    amount INT NOT NULL,
+    amount BIGINT NOT NULL,
     description VARCHAR(100) NULL,
     transaction_date TIMESTAMP NOT NULL,
 
